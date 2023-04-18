dbda48408c254377d9bde3923dfde64c8c936e90
Ricardo Varela <ricardomvvarela@gmail.com>
2023-04-18 01:11:35 -0300

    First Version

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# automacao-tela-cadastro
+# Automação de Tela de Cadastro
+
+Teste automatizado utilizando o Selenium WebDriver com a linguagem de programação Java e outras tecnologias.
+
+
+<b>Pré-Requisitos:</b>
+
+- Selenium 4.0.0;
+- Instalar o JDK 17.
+
+<b>Outras tecnologias utilizadas:</b>
+
+- IntelliJ IDEA;
+- Maven Repository;
+- TestNG;
+- JUnit.
+
+
+<b>Observação:</b> A versão do navegador precisa ser compatível com a versão do WebDriver para rodar os testes.
+Versão ChromeDriver utilizada: 111.0.5563.64
+
+<i><b>Versão:</b> <i>1.0.0</i>
---
+++ src/test/java/cadastro.tela/CadastroCamposTest.java
@@ -0,0 +1,71 @@
+package cadastro.tela;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class CadastroCamposTest {
+
+    private WebDriver driver;
+
+    @BeforeTest
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("remote-allow-origins=*");
+        System.setProperty("webdriver.chrome.driver", "C:/Driver/chromedriver.exe");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://netorgft4015814-my.sharepoint.com/:u:/g/personal/joao_vieck_warmupweb_com/ERI0uONwcihAgMqxIKzPw7QBcTh2M9B6DhlSJbp4dRasNg");
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();
+
+    }
+
+    @Test
+    public void CadastroPreenchimentoAll() throws InterruptedException {
+        Thread.sleep(1000);
+
+        driver.switchTo().frame(0);
+        WebElement nomeCompleto = driver.findElement(By.xpath("//input[@id='nome_completo']"));
+        WebElement telefoneUsuario = driver.findElement(By.xpath("//input[@id='telefone']"));
+        WebElement nomeRua = driver.findElement(By.xpath("//input[@id='nome_rua']"));
+        WebElement numeroCasa = driver.findElement(By.xpath("//input[@id='numero']"));
+        WebElement complemento = driver.findElement(By.xpath("//input[@id='complemento']"));
+        WebElement bairro = driver.findElement(By.xpath("//input[@id='bairro']"));
+        WebElement cidade = driver.findElement(By.xpath("//input[@id='cidade']"));
+        WebElement estado = driver.findElement(By.xpath("//input[@id='estado']"));
+        WebElement cepEndereco = driver.findElement(By.xpath("//input[@id='cep']"));
+        WebElement botaoSalvar = driver.findElement(By.xpath("//button[@type='submit']"));
+
+        nomeCompleto.sendKeys("Ricardo Teste");
+        telefoneUsuario.sendKeys("84999990000");
+        nomeRua.sendKeys("Rua Bom Pastor");
+        numeroCasa.sendKeys("2224");
+        complemento.sendKeys("6 Andar, Sala 611");
+        bairro.sendKeys("Ipiranga");
+        cidade.sendKeys("São Paulo");
+        estado.sendKeys("SP");
+        cepEndereco.sendKeys("04203022");
+
+        Thread.sleep(2000);
+
+        botaoSalvar.click();
+
+        Thread.sleep(4000);
+    }
+
+
+}
